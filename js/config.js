06a876346b916f2956e870a6e32bea2640cdcbd2
Ken Ng <wernken.ng@rea-group.com>
2020-09-08 15:26:54 +0800

    add cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_kywtYRNY1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3js3hmour2tmsn3m7bbhs1pk95', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
